741a1f5c5732ed2e014bff951243a69f923b8220
hchiam123 <57424985+hchiam123@users.noreply.github.com>
2019-11-06 10:27:25 -0500

    animate links for better UX

--- animation.js
+++ animation.js
@@ -9,3 +9,31 @@ $('a').css({
 });
 $('h1').css('position', 'relative').animate({'top': 0, 'left': 0}, 100);
 $('a').css('position', 'relative').animate({'left': 0}, 100);
+
+$('a, main p').on('click', function() {
+  var url = $(this).attr('href');
+  var newTab = $(this).attr('target');
+  $(this).removeAttr('href');
+  $(this).css({
+    transition: '0s',
+    color: 'transparent',
+  }).delay(100).queue(function(next) {
+    $(this).css({
+      'transition': '0.3s',
+      'z-index': 900,
+      'opacity': '0.5 !important',
+      'cursor': 'not-allowed !important',
+      'text-decoration': 'none !important;}',
+      'transform': 'scale(20)',
+    });
+    // next();
+    if (!url) return;
+    setTimeout(function() {
+      if (newTab) {
+        window.open(url, newTab);
+      } else {
+        window.location.href = url;
+      }
+    }, 10); 
+  })
+});
--- generate-links.js
+++ generate-links.js
@@ -1,25 +1,25 @@
-const path = window.location.pathname;
-const pathParts = path.split('/');
-const door = pathParts[pathParts.length-3];
-const subsection = pathParts[pathParts.length-1].replace(/\.html$/, '');
-const textFilePath = '../../../list-of-links/' + door + '/' + subsection + '.txt';
+var path = window.location.pathname;
+var pathParts = path.split('/');
+var door = pathParts[pathParts.length-3];
+var subsection = pathParts[pathParts.length-1].replace(/\.html$/, '');
+var textFilePath = '../../../list-of-links/' + door + '/' + subsection + '.txt';
 fetch(textFilePath)
 .then(function(response) {
   return response.text();
 })
 .then(function(text) {
-  const links = text.split('\n').filter(function(line) {
+  var links = text.split('\n').filter(function(line) {
     return line !== '';
   });
   links.forEach(function(link) {
     if (!link.startsWith('http://') && !link.startsWith('https://')) {
       link = 'https://' + link;
     }
-    const a = document.createElement('a');
+    var a = document.createElement('a');
     a.setAttribute('href', link);
     a.setAttribute('target', '_blank');
     a.innerHTML = link;
-    const p = document.createElement('p');
+    var p = document.createElement('p');
     p.appendChild(a);
     document.getElementsByTagName('main')[0].appendChild(p);
   });
